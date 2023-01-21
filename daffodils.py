05cbe406b54fd9bc76dfcd2f99f531e091df664e
Alfred Wilmot <alfred.wilmot@medtronic.com>
2023-01-21 20:04:52 +0000

    some generator functions to help with testing

--- daffodils.py
+++ daffodils.py
@@ -35,3 +35,36 @@ class Daffodil:
     """
     location: Target
     mature: bool
+
+
+def generate_random_daffodil(spawn_volume: GantryWorkspace) -> Daffodil:
+    """
+    Randomly places a Daffodil object with randomized attributes
+    within the specified spawn volume.
+    """
+    x = randint(
+        spawn_volume.x_min,
+        spawn_volume.x_max,
+    )
+    y = randint(
+        spawn_volume.y_min,
+        spawn_volume.y_max,
+    )
+    z = randint(
+        spawn_volume.z_min,
+        spawn_volume.z_max,
+    )
+
+    coord = Target(x, y, z)
+
+    return Daffodil(location=coord, mature=choice([True, False]))
+
+
+def generate_list_of_random_daffodils(
+    spawn_volume: GantryWorkspace,
+    list_length: int = 1,
+) -> list[Daffodil]:
+    """
+    Gimmie many pretty daffodils!
+    """
+    return [generate_random_daffodil(spawn_volume) for i in range(list_length)]
